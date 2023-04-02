f48b56e79e68e3063cab482992acbbb2c31d2faf
takinabradley <bradfinney98@gmail.com>
2023-04-02 17:16:45 -0400

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["prettier", "standard"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Firebase Todo
+
+A simple todo app that uses Firebase for data storage.
+
+## technologies used
+
+- Webpack
+- Babel
+- ES6 Modules
+- Jest
+- Sass
+- ESLint
+- Prettier
